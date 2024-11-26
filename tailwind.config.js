7e264081db18c06924a699d297e5d76133ed2b43
Afeez Bolaji <afeezbolajiola@gmail.com>
2024-11-26 13:24:05 +0100

    structuring the tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    container:{
+      center:true
+    },
+    colors: {
+      yellow: {
+        DEFAULT: "hsl(47, 88%, 63%)",
+      },
+      white:{
+        DEFAULT: "hsl(0, 0%, 100%)"
+      },
+      gray: {
+        light: "hsl(0, 0%, 42%)",
+        dark: "hsl(0, 0%, 7%)"
+      }
+    }
+  },
+  plugins: [],
+}
+
